2baa101acc125db930dd2195fe2048c9d7a8a8e7
Mollenthe4th <mollern2000@outlook.dk>
2021-11-18 23:10:29 +0100

    Writing a shell for the OS

---
+++ Shell/shell.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+
+
+int main(int argc, char **argv)
+{
+
+  // Run command loop.
+  shell_loop();
+
+  // Perform any shutdown/cleanup.
+
+  return 0;
+}
