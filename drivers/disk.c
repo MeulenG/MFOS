39563a02d316ac2300d671f0780a9f46b744dde1
Mollenthe4th <mollern2000@outlook.dk>
2021-11-23 22:42:47 +0100

    updated files

---
+++ drivers/disk.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include "disk.h"
+#include "../libc/mem.h"
+#include "../libc/string.h"
+
+void initdiskdriver() {
+
+}
+
+
+int main() {
+
+}
---
+++ drivers/disk.h
@@ -0,0 +1,4 @@
+#include <pthread.h>
+#include <stdint.h>
+#include "../libc/mem.h"
+#include "../libc/string.h"
