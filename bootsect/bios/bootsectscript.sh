2be8d41885dd63d0246ac410d9c0cc9328a512d8
Mollenthe4th <mollern2000@outlook.dk>
2022-02-22 10:57:00 +0100

    updated script to compile

--- bootsect/bios/bootsectscript.sh
+++ bootsect/bios/bootsectscript.sh
@@ -1,5 +1,7 @@
 nasm -f bin -o mbr.bin mbr.asm
 nasm -f bin -o Loader.bin Loader.asm
+nasm -f bin -o kernel_entry.bin kernel_entry.asm
 dd if=mbr.bin of=boot.img bs=512 count=1 conv=notrunc
 dd if=Loader.bin of=boot.img bs=512 count=5 seek=1 conv=notrunc
+dd if=kernel_entry.bin of=boot.img bs=512 count=100 seek=6 conv=notrunc
 qemu-system-x86_64 -cpu qemu64,pdpe1gb -drive format=raw,file=boot.img -D ./log.txt -monitor stdio -smp 1 -m 4096
