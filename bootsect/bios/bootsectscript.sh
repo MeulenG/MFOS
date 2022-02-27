a1a9b576afa0be7dc81215a065453edf0a511f25
Mollenthe4th <mollern2000@outlook.dk>
2022-02-27 18:02:40 +0100

    Updated boot script

--- bootsect/bios/bootsectscript.sh
+++ bootsect/bios/bootsectscript.sh
@@ -1,6 +1,9 @@
 nasm -f bin -o mbr.bin mbr.asm
 nasm -f bin -o Loader.bin Loader.asm
-nasm -f bin -o kernel_entry.bin kernel_entry.asm
+nasm -f elf64 -o kernel.o kernel_entry.asm
+gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone -c main_kernel.c
+ld -nostdlib -T link.lds -o kernel kernel.o main_kernel.o
+objcopy -O binary kernel kernel.bin
 dd if=mbr.bin of=boot.img bs=512 count=1 conv=notrunc
 dd if=Loader.bin of=boot.img bs=512 count=5 seek=1 conv=notrunc
 dd if=kernel_entry.bin of=boot.img bs=512 count=100 seek=6 conv=notrunc
