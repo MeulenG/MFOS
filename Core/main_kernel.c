cd5cb3a1a82dc5bbb430758319dfa81159520896
Mollenthe4th <mollern2000@outlook.dk>
2022-03-14 23:00:11 +0100

    Core

--- Core/main_kernel.c
+++ Core/main_kernel.c
@@ -1,14 +1,10 @@
 #include "../cpu/trap.h"
 #include "../libc/print.h"
-
-
+//#include "debug.h"
+#include "../libc/memory.h"
 
 void main_kernel(void)
 {
-   char *string = "Hello and Welcome my dude";
-   int64_t value = 0x123456789ABCD;
    init_idt();
-
-   printk("%s\n", string);
-   printk("This value is equal to %x", value);
+   init_memory();
 }
