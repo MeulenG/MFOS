b6fc8d6077f6e5519b98ea3e9a3a6f64836e46db
MeulenG <Mollern2000@outlook.com>
2022-11-07 23:41:15 +0100

    Writing a new bootloader that isn't hardcoded

--- SysBoot/Stage1_X86.asm
+++ SysBoot/Stage1_X86.asm
@@ -8,23 +8,23 @@ BITS	16							; We are in 16 bit Real Mode
 
 ORG		0x7C00						; We are loaded by BIOS at 0x7C00
 
-MAIN:       JMP STAGE1              ; Jump over the Fat32 Blocks
+MAIN:       JMP Entry              ; Jump over the Fat32 Blocks
 
 ; *************************
 ; FAT Boot Parameter Block
 ; *************************
 szOemName					DB		"MY    OS"
-wBytesPerSector				DW		0
-bSectorsPerCluster			DB		0
-wReservedSectors			DW		0
-bNumFATs					DB		0
+BPB_BytesPerSec				DW		257   ; Bytes Per Sector ; Offset 0x0B ; 16 Bits ; Value: Always 512 Bytes
+BPB_SecPerClus  			DB		1   ; Sectors Per Cluster ; Offset 0x0D ; 8 Bits ; Value: 1, 2, 4, 8, 16, 32, 64, 128
+BPB_RsvdSecCnt  			DW		257   ; Number of Reserved Sectors ; Offset 0x0E ; 16 Bits ; Value: 0x20
+BPB_NumFATs					DB		1   ; Number of FATs ; Offset 0x10 ; 8 Bits ; Value: Always 2
 wRootEntries				DW		0
-wTotalSectors				DW		0
-bMediaType					DB		0
-wSectorsPerFat				DW		0
-wSectorsPerTrack			DW		0
-wHeadsPerCylinder			DW		0
-dHiDDenSectors				DD 		0
+BPB_TotSec32				DD		16843009 ; Total Sectors
+BPB_Media					DB		1
+BPB_FATSz32 				DD		16843009   ; Sectors Per FAT ; Offset 0x24 ; 32 Bits ; Depends On Disk Size
+BPB_SecPerTrk   			DW		257
+BPB_NumHeads    			DW		257
+BPB_HiddSec 				DD 		16843009
 dTotalSectors				DD 		0
 
 ; *************************
@@ -33,7 +33,7 @@ dTotalSectors				DD 		0
 dSectorsPerFat32			DD 		0
 wFlags						DW		0
 wVersion					DW		0
-dRootDirStart				DD 		0
+BPB_RootClus				DD 		16843009   ; Root Directory First Cluster ; Offset 0x2C ; 32 Bits ; Value: 0x00000002
 wFSInfoSector				DW		0
 wBackupBootSector			DW		0
 
@@ -55,169 +55,79 @@ szFSName					DB		"FAT32   "
 ;***************************************
 
 PRINT16BIT:
-			LODSB					        ; load next byte from string from SI to AL
+			lodsb					        ; load next byte from string from SI to AL
 
-			OR			al, al		        ; Does AL=0?
+			or			al, al		        ; Does AL=0?
 			
-            JZ			PRINTDONE16BIT	    ; Yep, null terminator found-bail out
+            jz			PRINTDONE16BIT	    ; Yep, null terminator found-bail out
 			
-            MOV			ah,	0eh	            ; Nope-Print the character
+            mov			ah,	0eh	            ; Nope-Print the character
 			
-            INT			10h
+            int			10h
 			
-            JMP			PRINT16BIT		    ; Repeat until null terminator found
+            jmp			PRINT16BIT		    ; Repeat until null terminator found
 
 PRINTDONE16BIT:
 			
-            RET					            ; we are done, so return
+            ret					            ; we are done, so return
 
 ;*************************************************;
 ;	Bootloader Entry Point
 ;*************************************************;
+; dl = drive number
+; si = partition table entry
+Entry:
+    xchg bx, bx
+    cli ;disable interrupts
+    jmp 0:loadCS
 
-STAGE1:
-    ;----------------------------------------------------
-    ; code located at 0000:7C00, adjust segment registers
-    ;----------------------------------------------------
-    
-    CLI                                    ; disable interrupts
-    
-    XOR ax,ax   
-    
-    MOV ds,ax
-    
-    MOV es,ax  
-    
-    MOV ss,ax
-    
-    ;----------------------------------------------------
-    ; create stack
-    ;----------------------------------------------------
-    
-    MOV sp,0x7c00
-    
-    MOV         si, msgStart
-    
-    CALL        PRINT16BIT                     ; Error message
-    
-    STI                                    ; restore interrupts
+loadCS:
+    xor ax, ax
+    mov es, ax
+    mov ss, ax
+
+    ; we setup our stack, this is temporary for our stage1(512 bytes)
+    mov sp, 0x7C00
+    mov sp, ax
+    sti
+    cld
 
 
-TESTDISKEXTENTION:
-    
-    MOV [bPhysicalDriveNum],dl
-    
-    MOV ah,0x41
-    
-    MOV bx,0x55aa
-    
-    INT 0x13
-    
-    JC DEATHSCREEN
-    
-    CMP bx,0xaa55
-    
-    JNE DEATHSCREEN
 
-STAGE2LOADER:
-    ; DS:SI (segment:offset pointer to the DAP, Disk Address Packet)
-    ; DS is Data Segment, SI is Source Index
-    MOV         si,ReadPacket
-    ; size of Disk Address Packet (set this to 0x10)
-    MOV         word[si],0x10
-    ; number of sectors(loader) to read
-    MOV         word[si+2],5
-    ; number of sectors to transfer
-    ; transfer buffer (16 bit segment:16 bit offset)
-    ; 16 bit offset=0x7e00 (stored in word[si+4])
-    ; 16 bit segment=0 (stored in word[si+6])
-    ; address => 0 * 16 + 0x7e00 = 0x7e00
-    MOV         word[si+4],0x7e00
+ReadSectors:
+    mov bp, sp
+    push dx
     
-    MOV         word[si+6],0
-    ; absolute number of the start of the sectors to be read
-    ; LBA=1 (the 2nd sector) is the start of sector to be read
-    ; LBA=1 is start of loader sector
-    ; lower part of 64-bit starting LBA
-    MOV         DWord[si+8],1
-    ; upper part of 64-bit starting LBA
-    MOV         DWord[si+0xc],0
-    ; dl=drive id
-    MOV         dl,[bPhysicalDriveNum]
-    ; function code, 0x42 = Extended Read Sectors From Drive
-    MOV         ah,0x42
-    
-    INT         0x13
-    
-    JC          DEATH
+    ; Lets calculate the first data sector = bNumFATs * BPB_FATSZ32 + BPB_ResvdSecCnt
+    mov al, [BPB_NumFATs]
+    mov ebx, [BPB_FATSz32]
+    mul ebx
+    xor ebx, ebx
+    mov bx, [BPB_RsvdSecCnt]
+    add eax, ebx
+    ; Our ax register is our first data sector
+    push eax
 
-    MOV         dl,[bPhysicalDriveNum]
+    ; We need to get the location of the first FAT
+    ; FATsector = BPB_ResvdSecCnt
+    xor eax, eax
+    mov ax, [BPB_RsvdSecCnt]
+    push eax
 
-    MOV         si, msgStageTwo
+    ; BytsPerCluster = BPB_BytsPerSec * BPB_SecPerClus
+    mov ax, [BPB_BytesPerSec]
+    mov bx, [BPB_SecPerClus]
+    mul bx
+    push eax
     
-    CALL        PRINT16BIT                     ; Error message
-    
-    JMP         0x7e00
+    ; FATClusterMask = 0x0FFFFFFF
+    mov eax, 0x0FFFFFFF
+    push eax
 
-DEATH:
-DEATHSCREEN:
-    
-    MOV         si, msg
-    
-    CALL        PRINT16BIT                     ; Error message
-    
-    MOV         ah, 0x00
-    
-    INT         0x16                           ; SMACK YOUR ASS ON THAT KEYBOARD AGAIN
-    
-    INT         0x19                           ; Reboot and try again, bios uses int 0x19 to find a bootable device
+    ;; FATEoFMask = FATClusterMask & 0xFFFFFFF8
+	and al, 0xF8
+	push eax
 
-END:
-    HLT
-    
-    JMP END
-
-;*******************************************************
-;	Data Section
-;*******************************************************
-msg	                        DB	"Hahaha, Fuck you, but at stage1", 0
-MessageLenmsg:              equ $-msg
-ReadPacket:                 TIMES 16 DB 0
-msgStart                    DB 0x0D,0x0A, "Boot Loader starting (0x7C00)....", 0x0D, 0x0A, 0x00
-msgStageTwo                 DB "Jumping to stage2 (0x7E00)", 0x0D, 0x0A, 0x0D, 0x0A, 0x00
-
-; 0x1be = 446 bytes, which is bootloader code size
-; $-$$ is current section size (in bytes)
-TIMES (0x1BE-($-$$)) DB 0
-    ; here are partition table entries (which is total 64 bytes)
-    ; there are 4 entries and only define the first entry
-    ; each entry has 16 bytes (64/4=16)
-    ; this is the first partition entry format
-    ; boot indicator, 0x80=bootable partition
-    DB 80H
-    ; starting CHS
-    ; C=cylinder, H=head, S=sector
-    ; the range for cylinder is 0 through 1023
-    ; the range for head is 0 through 255 inclusive
-    ; The range for sector is 1 through 63
-    ; address of the first sector in partition
-    ; 0(head),2(sector),0(cylinder)
-    DB 0,2,0
-    ; partition type
-    DB 0F0H
-    ; ending of CHS
-    ; address of last absolute sector in partition
-    DB 0FFH,0FFH,0FFH
-    ; LBA of the first absolute sector
-    ; LBA = (C × HPC + H) × SPT + (S − 1)
-    ; S(sector)=2, C(cylinder)=0, H(head)=0 => LBA=1
-    DD 1
-    ; size (number of sectors in partition)
-    ; here is (20*16*63-1) * 512 bytes/ (1024 * 1024) = 10 mb
-    DD (20*16*63-1)
-	; other 3 entries are set to 0
-    TIMES (16*3) DB 0
-    ; Boot Record signature
-    ; here is the same as dw 0aa55h
-    ; little endian
-    DW 0xAA55                              ; fill out the last 2 bytes of our stage1 bootloader
+ImageName   db "KRNLDR  SYS"
+times 510-($-$$) DB 0
+dw 0xAA55
