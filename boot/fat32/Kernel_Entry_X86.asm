429ef922053eed65ac9b5a71aea571d048fc9048
MeulenG <Olivermeulengracht@protonmail.com>
2024-12-03 19:29:19 +0100

    No need to load in gdt or tss, we already did it in protected mode

--- boot/fat32/Kernel_Entry_X86.asm
+++ boot/fat32/Kernel_Entry_X86.asm
@@ -1,8 +1,48 @@
-BITS 64
+section .text
+extern kernel_main
 global _start
 
-_start:
-    mov rsp, 0x200000
+_start:    
+InitPIT:
+    mov al, (1<<2)|(3<<4)
+    out 0x43, al
 
+    mov ax, 11931
+    out 0x40, al
+    mov al, ah
+    out 0x40, al
+
+InitPIC:
+    mov al, 0x11
+    out 0x20, al
+    out 0xa0, al
+
+    mov al, 32
+    out 0x21, al
+    mov al, 40
+    out 0xa1, al
+
+    mov al, 4
+    out 0x21, al
+    mov al, 2
+    out 0xa1, al
+
+    mov al, 1
+    out 0x21, al
+    out 0xa1, al
+
+    mov al, 11111110b
+    out 0x21, al
+    mov al, 11111111b
+    out 0xa1, al
+
+    push 8
+    push KernelEntry
+    db 0x48
+    retf
+KernelEntry:
+    mov rsp, 0x200000
+    call kernel_main
 hang:
     hlt
+    jmp hang
