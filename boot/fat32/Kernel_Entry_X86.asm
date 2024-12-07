2ee76738ad43dabd58c3edde15f16756232462a7
MeulenG <Olivermeulengracht@protonmail.com>
2024-12-07 18:38:18 +0100

    Crashes when loading Tss

--- boot/fat32/Kernel_Entry_X86.asm
+++ boot/fat32/Kernel_Entry_X86.asm
@@ -1,8 +1,47 @@
+section .data
+
+Gdt64:
+    dq 0
+    dq 0x0020980000000000
+    dq 0x0020f80000000000
+    dq 0x0000f20000000000
+
+TssDesc:
+    dw TssLen-1
+    dw 0
+    db 0
+    db 0x89
+    db 0
+    db 0
+    dq 0
+Gdt64Len: equ $-Gdt64
+Gdt64Ptr: 
+    dw Gdt64Len-1
+    dq Gdt64
+Tss:
+    dd 0
+    dq 0x190000
+    times 88 db 0
+    dd TssLen
+TssLen: equ $-Tss
 section .text
 extern kernel_main
 global _start
 
-_start:    
+_start:
+    lgdt [Gdt64Ptr]
+
+SetTss:
+    mov rax, Tss
+    mov [TssDesc+2],ax
+    shr rax,16
+    mov [TssDesc+4],al
+    shr rax,8
+    mov [TssDesc+7],al
+    shr rax,8
+    mov [TssDesc+8],eax
+    mov ax,0x20
+    ltr ax
 InitPIT:
     mov al, (1<<2)|(3<<4)
     out 0x43, al
@@ -40,8 +79,11 @@ InitPIC:
     push KernelEntry
     db 0x48
     retf
+
 KernelEntry:
+    cli
     mov rsp, 0x200000
+    sub esp, 8
     call kernel_main
 hang:
     hlt
