fc936de93a7d6f43bab2c76b7678c8c97f54329d
MeulenG <Olivermeulengracht@protonmail.com>
2024-12-08 15:05:34 +0100

    many: Correctly loads and jumps to my C function

--- boot/fat32/Kernel_Entry_X86.asm
+++ boot/fat32/Kernel_Entry_X86.asm
@@ -1,90 +1,19 @@
-section .data
-
-Gdt64:
-    dq 0
-    dq 0x0020980000000000
-    dq 0x0020f80000000000
-    dq 0x0000f20000000000
-
-TssDesc:
-    dw TssLen-1
-    dw 0
-    db 0
-    db 0x89
-    db 0
-    db 0
-    dq 0
-Gdt64Len: equ $-Gdt64
-Gdt64Ptr: 
-    dw Gdt64Len-1
-    dq Gdt64
-Tss:
-    dd 0
-    dq 0x190000
-    times 88 db 0
-    dd TssLen
-TssLen: equ $-Tss
 section .text
-extern kernel_main
+extern kMain
 global _start
 
 _start:
-    lgdt [Gdt64Ptr]
+	;We disable interrupts, we have no IDT installed
+	cli
+    
+	; Place the vboot header into first argument for kMain
+	mov rcx, rbx
 
-SetTss:
-    mov rax, Tss
-    mov [TssDesc+2],ax
-    shr rax,16
-    mov [TssDesc+4],al
-    shr rax,8
-    mov [TssDesc+7],al
-    shr rax,8
-    mov [TssDesc+8],eax
-    mov ax,0x20
-    ltr ax
-InitPIT:
-    mov al, (1<<2)|(3<<4)
-    out 0x43, al
+	;Now call the init function
+    sub rsp, 0x20
+	call kMain
+	mov rax, 0x000000000000DEAD
 
-    mov ax, 11931
-    out 0x40, al
-    mov al, ah
-    out 0x40, al
-
-InitPIC:
-    mov al, 0x11
-    out 0x20, al
-    out 0xa0, al
-
-    mov al, 32
-    out 0x21, al
-    mov al, 40
-    out 0xa1, al
-
-    mov al, 4
-    out 0x21, al
-    mov al, 2
-    out 0xa1, al
-
-    mov al, 1
-    out 0x21, al
-    out 0xa1, al
-
-    mov al, 11111110b
-    out 0x21, al
-    mov al, 11111111b
-    out 0xa1, al
-
-    push 8
-    push KernelEntry
-    db 0x48
-    retf
-
-KernelEntry:
-    cli
-    mov rsp, 0x200000
-    sub esp, 8
-    call kernel_main
-hang:
-    hlt
-    jmp hang
+	.idle:
+		hlt
+		jmp .idle
